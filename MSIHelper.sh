8d864a796625d11bd27d16ac2d8fd26bb92b9567
antoineatstariongroup <a.theate@stariongroup.eu>
2024-12-13 08:53:17 +0100

    MSIHelper doc

--- MSIHelper.sh
+++ MSIHelper.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+:'
+ This script lists all DLL file name that are present inside a build directory, excluding the EAModelKit.dll file.
+ This feature should be use before releasing a new version of the MSI installer. 
+ This script can be used like : bash MSIHelper.sh './EA-ModelKit/bin/Release/net481'
+ The AppComponents.wxs file have to be updated with the content of the output of the script (present in the app_components.xml). Do not update the first part of this file, that is related to the EAModelKit class registration.
+'
 
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <build_target_directory_path>"
